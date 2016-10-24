2a214039c16b73b2c96f6f18ffd2c2bd2b51afa6
Rajashree Baradur <rajashreebaradur@Rajashrees-MacBook-Pro.local>
2016-10-24 10:30:15 -0400

    committing plot2
    committing plot2

--- Exploratory_Analysis_4/Course_Project/plot2.R
+++ Exploratory_Analysis_4/Course_Project/plot2.R
@@ -8,8 +8,11 @@ setwd("~/datasciencecoursera/Exploratory_Analysis_4/Course_Project")
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+#subset emissions of Baltimore
+sub1 <- subset(NEI, fips=="24510")
+
 #Aggregate emissions by year
-dataYear <- with(NEI, aggregate(Emissions, by = list(year), sum))
+md <- tapply(sub1$Emissions, sub1$year, sum)
 
 #plotting the data
-plot(dataYear, main = "Total PM2.5 Emissions by Year", xlab = "Year", ylab = "PM2.5 emissions", pch=19, type = 'b', lty=6, col="red")
+plot(md, main = "Total PM2.5 Emissions in Baltimore County", xlab = "Year", ylab = "PM2.5 emissions", pch=19, type = 'b', lty=6, col="red")
