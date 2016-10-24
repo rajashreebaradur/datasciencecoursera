495c60fa8129d663cdb90c7e35cee5623bd1638c
Rajashree Baradur <rajashreebaradur@Rajashrees-MacBook-Pro.local>
2016-10-24 11:53:11 -0400

    committing plot5
    committing plot5

---
+++ Exploratory_Analysis_4/Course_Project/plot5.R
@@ -0,0 +1,21 @@
+library(ggplot2)
+
+#Get current working directory
+getwd()
+
+#Set current working directory
+setwd("~/datasciencecoursera/Exploratory_Analysis_4/Course_Project")
+
+#Read data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#subset and aggregate emissions of Baltimore by motor vehicles
+sub2 <- subset(NEI, fips=="24510" & type=="ON-ROAD")
+
+balt_motor <- aggregate(sub2[c("Emissions")], list(year = sub2$year, type = sub2$type), sum)
+
+qplot(year, Emissions, data = balt_motor, 
+      geom= "line", 
+      main="Motor Vehicle-Related Emissions in Baltimore County", 
+      xlab="Year", ylab="Emission Levels")
